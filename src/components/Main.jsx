529450ecb29dbdc993579f048cfe8cb085d82d3c
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2021-08-15 19:20:29 +0900

    fixed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,38 +4,41 @@ const Main = () => {
     return(
     <section className = "section">
 
-<article class="message is-link">
-  <div class="message-header">
-    <p>英単語を楽しんで覚えよう</p>
-   
-  </div>
-  <div class="message-body">
-    このアプリでできること <br/>
-    <ul>
-        <li>・単語帳の作成や英単語の追加</li>
-        <li>・英単語を追加するときの翻訳機能</li>
-        <li>・単語帳をスライドして鑑賞</li>
-        <li>・作成した単語帳に対して、四択や記述式でクイズ</li>
-        <li>・クイズを解き続けることで実績を解除</li>
-    </ul>
-  </div>
-</article>
+        <article class="message is-link">
+            <div class="message-header">
+                <p>英単語を楽しんで覚えよう</p>
+            </div>
 
-<article class="message is-link">
-  <div class="message-header">
-    <p>スマホアプリとして起動してみよう</p>
+            <div class="message-body">
+                このアプリでできること <br/>
+                <ul>
+                    <li>・単語帳の作成や英単語の追加</li>
+                    <li>・英単語を追加するときの翻訳機能</li>
+                    <li>・単語帳をスライドして鑑賞</li>
+                    <li>・作成した単語帳に対して、四択や記述式でクイズ</li>
+                    <li>・クイズを解き続けることで実績を解除</li>
+                </ul>
+            </div>
+        </article>
+        
+        <article class="message is-link">
+            <div class="message-header">
+                <p>スマホアプリとして起動してみよう</p>
+            </div>
+            
+            <div class="message-body">
+                このアプリはionicで作成されていて、PWAによってネイティブアプリのように操作できる。<br/><br/>
 
-  </div>
-  <div class="message-body">
-      iosの場合 <br/>
-      楽して覚える単語帳のページに移動 →「共有」アイコンをタップ → 「ホーム画面に追加」をタップ <br/> <br/>
-
-      Androidの場合 <br/>
-      楽して覚える単語帳のページに移動 →右上の「メニューアイコン」をタップ → 「ホーム画面に追加」をタップ <br/> <br/>
-
-      詳しくは<a href="https://www.ipodwave.com/iphone/howto/website_home.html" target="_blank">iosはこちら</a>，<a href="https://color-variations.com/android-siteicon" target="_blank">Androidはこちら</a>
-  </div>
-</article>
+                iosの場合 <br/>
+                楽して覚える単語帳のページに移動 →「共有」アイコンをタップ → 「ホーム画面に追加」をタップ <br/> <br/>
+            
+            
+                Androidの場合 <br/>
+                楽して覚える単語帳のページに移動 →右上の「メニュー」アイコンをタップ → 「ホーム画面に追加」をタップ <br/> <br/>
+            
+                詳しくは<a href="https://www.ipodwave.com/iphone/howto/website_home.html" target="_blank">iosはこちら</a>，<a href="https://color-variations.com/android-siteicon" target="_blank">Androidはこちら</a>
+            </div>
+        </article>
 
         <p className = "title  is-italic  is-family-primary">LINKS</p>
         <div className = "container">
