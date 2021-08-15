e75be72047ce02b037449d2279e2514221e5ccde
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2021-08-15 12:15:47 +0900

    add mend

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,6 @@
+
+const introduction = () => {
+
+}
+
+export default introduction;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,42 +1,64 @@
 
 const Main = () => {
+    const urls = [
+
+    ];
     return(
     <section className = "section">    
     <div className = "container">   
         <div className="columns is-vcentered">
-
             <div className="column is-2 ">
                 <div className = "card hero is-link">
-                    作品へのリンク
-                </div>    
-            </div>
+                <header class="card-header is-white">
+                    <p class="card-header-title is-white">
+                        <p className="title is-white"><a href="https://bulma.io/documentation/elements/button/">作品</a></p>
+                    </p>
+                </header>        
+            </div>    
+        </div>  
 
-            <div className="column is-2 ">
+           <div className="column is-2 ">
                 <div className = "card hero is-link">
-                    githubへのリンク
+                <header class="card-header is-white">
+                    <p class="card-header-title is-white">
+                        <p className="title is-white">github</p>
+                    </p>
+                </header>        
                 </div>    
-            </div>
+           </div>  
 
-            <div className="column is-2">
+           <div className="column is-3 ">
                 <div className = "card hero is-link">
-                    ポスターへのリンク
+                <header class="card-header is-white">
+                    <p class="card-header-title is-white">
+                        <p className="title is-white">作品動画</p>
+                    </p>
+                </header>        
                 </div>    
-            </div>
+           </div>  
 
-            <div className="column is-2">
+           <div className="column is-3 ">
                 <div className = "card hero is-link">
-                    動画へのリンク
+                <header class="card-header is-white">
+                    <p class="card-header-title is-white">
+                        <p className="title is-white">ポスター</p>
+                    </p>
+                </header>        
                 </div>    
-            </div>
-
-            <div className="column is-2">
+           </div>  
+        
+           <div className="column is-2 ">
                 <div className = "card hero is-link">
-                    texへのリンク
+                <header class="card-header is-white">
+                    <p class="card-header-title is-white">
+                        <p className="title is-white">tex</p>
+                    </p>
+                </header>        
                 </div>    
-            </div>
+           </div>  
 
             </div>
-            
+
         </div> 
      </section>
     );
