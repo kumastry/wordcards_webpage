c02aa3b65eba62faa6fe81dd31e2cd6a4e89de22
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2021-08-15 11:35:18 +0900

    vertex

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Main from './components/Main';
 
 function App() {
     return (
         
     <div>
         <Header/>
-
-        <div class="columns is-link">
-  <div class="column is-1 hero is-link">
-    fdfdfadfdasfadsfadsfdsf
-    dfasdfdasfdsfdsfadsfadsf
-    dfdsfadsfasddfadssfadsfds
-    fdfdsafasdfdassfds
-  </div>
-  <div class="column is-1 hero is-link ">
-    Second column
-  </div>
-  <div class="column  is-1 hero is-link">
-    Third column
-  </div>
-  <div class="column is-1  hero is-link">
-    Fourth column
-  </div>
-</div>
-
-<Footer/>
+        <Main/>
+        <Footer/>
     </div>
     );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,45 @@
 
 const Main = () => {
-    return();
+    return(
+    <section className = "section">    
+    <div className = "container">   
+        <div className="columns is-vcentered">
+
+            <div className="column is-2 ">
+                <div className = "card hero is-link">
+                    作品へのリンク
+                </div>    
+            </div>
+
+            <div className="column is-2 ">
+                <div className = "card hero is-link">
+                    githubへのリンク
+                </div>    
+            </div>
+
+            <div className="column is-2">
+                <div className = "card hero is-link">
+                    ポスターへのリンク
+                </div>    
+            </div>
+
+            <div className="column is-2">
+                <div className = "card hero is-link">
+                    動画へのリンク
+                </div>    
+            </div>
+
+            <div className="column is-2">
+                <div className = "card hero is-link">
+                    texへのリンク
+                </div>    
+            </div>
+
+            </div>
+            
+        </div> 
+     </section>
+    );
 }
 
 export default Main;
